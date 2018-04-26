9a4926eeeee5e597e039ae2e1e881dcafe693299
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2018-04-27 00:07:21 +0900

    Update GetStock.py

--- GetStock.py
+++ GetStock.py
@@ -4,14 +4,15 @@
 # Detail        :Script to obtain stock information from FRED.           #
 #                https://fred.stlouisfed.org/                            #
 # Implementer   :R.Ishikawa                                              #
-# Version     	:1.2                                                     #
-# Last update   :2018/2/24                                               #
+# Version     	:1.3                                                     #
+# Last update   :2018/4/26                                               #
 ##########################################################################
 
 #Version History
-#1 Create New                                    2017/10/1   R.I    Ver.1.0
-#2 Add Indices, NIKKEI225 and DJIA               2017/12/10  R.I    Ver.1.1
-#3 Add function to send gmail attached png file  2018/2/24   R.I    Ver.1.2
+#1 Create New                                                   2017/10/1   R.I    Ver.1.0
+#2 Add Indices, NIKKEI225 and DJIA                              2017/12/10  R.I    Ver.1.1
+#3 Add function to send gmail attached png file                 2018/2/24   R.I    Ver.1.2
+#4 Be able to receive both JPN/USD and NIKKEI225/DJIA data      2018/4/26   R.I    Ver.1.3
 
 #%matplotlib inline
 
@@ -30,7 +31,7 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
 class sendGmailAttach:
-    username, password = 'XXXXXXXXXXXX@gmail.com', 'XXXXXXXXXXXXXX'
+    username, password = 'seki.ishikawa@gmail.com', 'seki14kawa'
 
     def __init__(self, to, sub, body, attach_file):
         host, port = 'smtp.gmail.com', 465
@@ -74,23 +75,33 @@ plt.ylabel("Yen / Doller")
 plt.savefig("JPNUSD.png", bbox_inches="tight")
 
 #2 Add NIKKEI225 and DJIA Start
-#df2 = web.DataReader(["NIKKEI225", "DJIA"], "fred", start, end)
+df2 = web.DataReader(["NIKKEI225", "DJIA"], "fred", month_ago, today)
 
-#df2.plot()
+df2.plot()
 
-#plt.xlabel("Date")
-#plt.ylabel("NIKKEI225/DJIA")
-#plt.savefig("NIKKEI225-DJIA.png", bbox_inches="tight")
+plt.xlabel("Date")
+plt.ylabel("NIKKEI225/DJIA")
+plt.savefig("NIKKEI225-DJIA.png", bbox_inches="tight")
 #2 Add NIKKEI225 and DJIA End
 
 #3 Add Main ##################################################################
 
 if __name__ == '__main__':
-    to = 'XXXXXXXXXXXXXXXX@gmail.com' # To:
-    sub = '[GetStock] Todays JPN/USD' # mail title
+    to = 'seki.ishikawa@gmail.com' # To:
+    sub_JPNUSD = '[GetStock] Todays JPN/USD' #4 mail title for JPN/USD
+    sub_NIKKEI225_DJIA = '[GetStock] Todays NIKKEI225-DJIA' #4 mail title for NIKKEI225-DJIA
     body = '' # body
 
-    attach_file = {'name':'JPNUSD.png', 'path': '/Users/RI/Desktop/Code/tool/GetStock/JPNUSD.png'}
-    sendGmailAttach(to, sub, body, attach_file)
+    attach_file = {'name':'JPNUSD.png', 'path': '/Users/RyoISHIKAWA/Desktop/Code/tool/GetStock/JPNUSD.png'}
+    
+    #4 Add attached file, 'NIKKEI225-DJIA.png' start
+    attach_file2 = {'name':'NIKKEI225-DJIA.png','path': '/Users/RyoISHIKAWA/Desktop/Code/tool/GetStock/NIKKEI225-DJIA.png'}
+    #4 Add attached file, 'NIKKEI225-DJIA.png' end
+    
+    sendGmailAttach(to, sub_JPNUSD, body, attach_file)
+    
+    #4 Add sending precedure for NIKKEI225/DJIA start
+    sendGmailAttach(to, sub_NIKKEI225_DJIA, body, attach_file2)
+    #4 Add sending precedure for NIKKEI225/DJIA end
 
 ##############################################################################
