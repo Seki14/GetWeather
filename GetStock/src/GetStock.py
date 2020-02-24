3c12c8f2f1fe3b0f0184e13969e092548fc198e8
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2020-02-24 22:40:36 +0900

    Update GetStock.py

--- GetStock/src/GetStock.py
+++ GetStock/src/GetStock.py
@@ -4,93 +4,74 @@
 # Detail        :Script to obtain stock information from FRED.           #
 #                https://fred.stlouisfed.org/                            #
 # Implementer   :R.Ishikawa                                              #
-# Version     	:1.2                                                     #
-# Last update   :2018/2/24                                               #
+# Version     	:1.4                                                     #
+# Last update   :2020/2/24                                               #
 ##########################################################################
 
 #Version History
-#1 Create New                                    2017/10/1   R.I    Ver.1.0
-#2 Add Indices, NIKKEI225 and DJIA               2017/12/10  R.I    Ver.1.1
-#3 Add function to send gmail attached png file  2018/2/24   R.I    Ver.1.2
+#1 Create New                                                   2017/10/1   R.I    Ver.1.0
+#2 Add Indices, NIKKEI225 and DJIA                              2017/12/10  R.I    Ver.1.1
+#3 Add function to send gmail attached png file                 2018/2/24   R.I    Ver.1.2
+#4 Be able to receive both JPN/USD and NIKKEI225/DJIA data      2018/4/26   R.I    Ver.1.3
+#5 Chagne mail notify to LINE Notify                            2020/2/24   R.I    Ver.1.4
 
 #%matplotlib inline
 
 import datetime
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
-#3 import dateutil Module
 from dateutil.relativedelta import relativedelta
+import requests
 
-
-#3 Add Function to send gmail #############################################
-from email.mime.text import MIMEText
-import smtplib
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-
-class sendGmailAttach:
-    username, password = 'YOUR GMAIL ADDRESS', 'PASSWORD'
-
-    def __init__(self, to, sub, body, attach_file):
-        host, port = 'smtp.gmail.com', 465
-        msg = MIMEMultipart()
-        msg['Subject'] = sub
-        msg['From'] = self.username
-        msg['To'] = to
-
-        # mail main
-        body = MIMEText(body)
-        msg.attach(body)
-
-        # Set Attachment
-        attachment = MIMEBase('image', 'png')
-        file = open(attach_file['path'], 'rb+')
-        attachment.set_payload(file.read())
-        file.close()
-        encoders.encode_base64(attachment)
-        attachment.add_header("Content-Disposition", "attachment", filename=attach_file['name'])
-        msg.attach(attachment)
-
-        smtp = smtplib.SMTP_SSL(host, port)
-        smtp.ehlo()
-        smtp.login(self.username, self.password)
-        smtp.mail(self.username)
-        smtp.rcpt(to)
-        smtp.data(msg.as_string())
-        smtp.quit()
-        
-#############################################################################
-
-#3 Set data range automatically
+# Set data range automatically
 today = datetime.datetime.today()
-month_ago = today - relativedelta(months=1)
+month_ago1 = today - relativedelta(months=1)
+month_ago2 = today - relativedelta(months=2)
 
-df = web.DataReader(["DEXJPUS"], "fred", month_ago, today)
-df.plot()
+# DEXJPUS Start (DISABLE)
+#df = web.DataReader(["DEXJPUS"], "fred", month_ago2, today)
+#df.plot()
 
-plt.xlabel("Date")
-plt.ylabel("Yen / Doller")
-plt.savefig("JPNUSD.png", bbox_inches="tight")
+#plt.xlabel("Date")
+#plt.ylabel("Yen / Doller")
+#plt.savefig("JPNUSD.png", bbox_inches="tight")
+# DEXJPUS End
 
-#2 Add NIKKEI225 and DJIA Start
-#df2 = web.DataReader(["NIKKEI225", "DJIA"], "fred", start, end)
-
+# DJIA Start (DISABLE)
+#df2 = web.DataReader(["DJIA"], "fred", month_ago1, today)
 #df2.plot()
 
 #plt.xlabel("Date")
-#plt.ylabel("NIKKEI225/DJIA")
-#plt.savefig("NIKKEI225-DJIA.png", bbox_inches="tight")
-#2 Add NIKKEI225 and DJIA End
+#plt.ylabel("DJIA")
+#plt.savefig("DJIA.png", bbox_inches="tight")
+# DJIA End
 
-#3 Add Main ##################################################################
+# NIKKEI225 Start
+df3 = web.DataReader(["NIKKEI225"], "fred", month_ago2, today)
+df3.plot()
 
-if __name__ == '__main__':
-    to = 'INPUT YOUR GMAIL ADDRESS' # To:
-    sub = '[GetStock] Todays JPN/USD' # mail title
-    body = '' # body
+plt.xlabel("Date")
+plt.ylabel("NIKKEI225")
+plt.savefig("NIKKEI225.png", bbox_inches="tight")
 
-    attach_file = {'name':'JPNUSD.png', 'path': '/CURRENT_DIR/JPNUSD.png'}
-    sendGmailAttach(to, sub, body, attach_file)
+# NIKKEI225 End
+
+# Execute LINE NOTIFY ########################################################
+
+line_notify_token = 'LINE_NOTIFY_TOKEN'
+line_notify_api = 'https://notify-api.line.me/api/notify'
+
+#message = '\n' + "(1)ドル円相場" + '\n' + "(2)ダウ平均株価" + '\n' + "(3)日経平均株価"
+message = today.strftime('%m/%d') + "時点の日経平均株価"
+payload = {'message': message}
+headers = {'Authorization': 'Bearer ' + line_notify_token}
+
+#files = {'imageFile': open("/home/pi/Ryo/tools/GetStock/JPNUSD.png","rb")}
+#files2 =  {'imageFile': open("/home/pi/Ryo/tools/GetStock/DJIA.png","rb")}
+files3 =  {'imageFile': open("/home/pi/Ryo/tools/GetStock/NIKKEI225.png","rb")}
+
+#line_notify = requests.post(line_notify_api, data=payload, headers=headers, files=files)
+#line_notify = requests.post(line_notify_api, data=payload, headers=headers, files=files2)
+line_notify = requests.post(line_notify_api, data=payload, headers=headers, files=files3)
 
 ##############################################################################
