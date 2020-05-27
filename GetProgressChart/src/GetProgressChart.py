f08eed6aea2f262e8ef1081a68ea5b45f766e61e
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2020-05-27 17:53:41 +0900

    Update GetProgressChart.py

--- GetProgressChart/src/GetProgressChart.py
+++ GetProgressChart/src/GetProgressChart.py
@@ -2,29 +2,19 @@
 # Module name       :GetProgressChart.py
 # Detail            :This's the script to make Progress Chart.
 # Implementer       :R.Ishikawa
-# Version           :1.0
+# Version           :1.1
 # Last update       :2020/05/23
+
+# HISTORY
+#1 Create New                  Ver.1.0  R.I  2020/05/23
+#2 Change Input format         Ver.1.1  R.I  2020/05/27
 
 import openpyxl.utils
 import os
-import sys
 
 from openpyxl.styles import PatternFill
 from openpyxl.styles.borders import Border, Side
 from openpyxl.formatting.rule import FormulaRule
-
-args = sys.argv
-argc = len(args)
-
-# The procedure in the case arguments' length is not enough
-if(argc != 4):
-   print('')
-   print('GetProgressChart Usage:')
-   print('args[1]: Set Start Year.')
-   print('args[2]: Set Start Month')
-   print('args[3]: Set Start Day')
-   print('')
-   quit()
 
 wb = openpyxl.Workbook()
 sheet = wb.active
@@ -37,10 +27,11 @@ def cellwidth(start, end, wide):
         sheet.column_dimensions['{}'.format(alpha)].width = wide
 
 
-cellwidth(1, 7, 8.38)
-cellwidth(2, 3, 17.38)
-cellwidth(8, 9, 0.38)
-cellwidth(9, 335, 4.88)
+cellwidth(1, 7, 8.38)   # A-F列セルのサイズ設定
+cellwidth(7, 8, 40)     # G列セルのサイズ設定
+cellwidth(2, 3, 17.38)  # B列セルのサイズ設定
+cellwidth(8, 9, 0.38)   # H列セルのサイズ設定
+cellwidth(9, 335, 4.88) # I-LV列セルのサイズ設定
 
 for rows in range(1, 100):
     sheet.row_dimensions[rows].height = 19
@@ -64,11 +55,15 @@ for rows in sheet.iter_rows(min_row=1, min_col=9, max_row=100, max_col=335):
             cell.border = Border(top=side3, right=side3, bottom=side3, left=side3)
 
 
-# load StartYear, StartMonth, and StartDay from args with ZEROSUPPRESS
-startYear = int(args[1])
-startMonth = int(args[2])
-startDay = int(args[3])
+# 同じ階層のディレクトリからproperty.txtをオープン
+property_data = open("property.txt", "r")
 
+# property.txtを1行ずつ要素として読み込み、PROPERTYリストを作成
+PROPERTY = property_data.readlines()
+
+startYear = int(PROPERTY[0])  # PROPERTYリストの1つ目の値を引用
+startMonth = int(PROPERTY[1]) # PROPERTYリストの2つ目の値を引用
+startDay = int(PROPERTY[2])   # PROPERTYリストの3つ目の値を引用
 
 sheet["B5"].value = "タスク"
 sheet["C4"].value = "着手日"
@@ -83,9 +78,9 @@ sheet["J2"].value = "年"
 sheet["I3"].value = startMonth
 sheet["J3"].value = "月"
 
-sheet["I4"].number_format = "d" #d = DATE
+sheet["I4"].number_format = "d"
 sheet["I4"].value = '=DATE(I2, I3, {})'.format(startDay)
-sheet["I5"].number_format = "aaa" #aaa = DAY OF THE WEEK
+sheet["I5"].number_format = "aaa"
 sheet["I5"].value = '=I4'
 
 for rows in sheet.iter_rows(min_row=4, min_col=10, max_row=5, max_col=334):
@@ -104,13 +99,13 @@ for rows in sheet.iter_rows(min_row=3, min_col=11, max_row=3, max_col=334):
                                                       sheet.cell(column=cell.column, row=cell.row + 1).coordinate)
 
 
-def fillColor(types, start, end):
+def colorMake(types, start, end):
     return PatternFill(fill_type=types, start_color=start, end_color=end)
 
 
-grayfill = fillColor('solid', 'd3d3d3', 'd3d3d3')
-bluefill = fillColor('solid', '1e90ff', '1e90ff') # Color of Planned Schedule
-ｍagentafill = fillColor('solid', 'e4007f', 'e4007f') # Color of Actual Schedule
+grayfill = colorMake('solid', 'd3d3d3', 'd3d3d3')
+planfill = colorMake('solid', PROPERTY[3], PROPERTY[3]) #計画セル色。PROPERTYリストの4つ目の値を引用
+actualfill = colorMake('solid', PROPERTY[4], PROPERTY[4]) #実績セル色。PROPERTYリストの5つ目の値を引用
 
 for rows in sheet.iter_rows(min_row=6, min_col=2, max_row=100, max_col=7):
     for cell in rows:
@@ -119,10 +114,10 @@ sheet.conditional_formatting.add('C6:G100', FormulaRule(formula=['NOT($F6="")'],
 
 sheet.conditional_formatting.add('I4:ME100', FormulaRule(formula=['OR(WEEKDAY(I$5)=1, WEEKDAY(I$5)=7)'], stopIfTrue=True, fill=grayfill))
 
-sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($D6<=I$4, $F6>=I$4)'], stopIfTrue=True, fill=ｍagentafill))
-sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($C6<=I$4, $E6>=I$4)'], stopIfTrue=True, fill=bluefill))
+sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($D6<=I$4, $F6>=I$4)'], stopIfTrue=True, fill=actualfill))
+sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($C6<=I$4, $E6>=I$4)'], stopIfTrue=True, fill=planfill))
 
 sheet.freeze_panes = 'I6'
 
-desktop_path = str(input('Input absolute path you want to save sheet >'))
-wb.save(desktop_path + 'ProgressChart.xlsx')
+desktop_path = PROPERTY[5] # PROPERTYリストの6つ目の値を引用
+wb.save(desktop_path + '進捗管理表.xlsx')
