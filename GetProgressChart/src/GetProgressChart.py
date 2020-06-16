45635e53dc5617e21e09d3c8fb645b3d4b3890a4
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2020-06-16 22:32:40 +0900

    Update GetProgressChart.py

--- GetProgressChart/src/GetProgressChart.py
+++ GetProgressChart/src/GetProgressChart.py
@@ -1,13 +1,14 @@
-# Tool name         :GetProgressChart
+# Tool name         :GetProgressChart(For Distribute)
 # Module name       :GetProgressChart.py
 # Detail            :This's the script to make Progress Chart.
 # Implementer       :R.Ishikawa
-# Version           :1.1
-# Last update       :2020/05/23
+# Version           :1.2
+# Last update       :2020/06/16
 
 # HISTORY
-#1 Create New                  Ver.1.0  R.I  2020/05/23
-#2 Change Input format         Ver.1.1  R.I  2020/05/27
+#1 Create New                                  Ver.1.0  R.I  2020/05/27
+#2 Add procedure to be easy to use in Win10    Ver.1.1  R.I  2020/06/14
+#3 Add 2 columns "No." and "担当者"            Ver.1.2  R.I  2020/06/16
 
 import openpyxl.utils
 import os
@@ -15,6 +16,7 @@ import os
 from openpyxl.styles import PatternFill
 from openpyxl.styles.borders import Border, Side
 from openpyxl.formatting.rule import FormulaRule
+
 
 wb = openpyxl.Workbook()
 sheet = wb.active
@@ -27,11 +29,11 @@ def cellwidth(start, end, wide):
         sheet.column_dimensions['{}'.format(alpha)].width = wide
 
 
-cellwidth(1, 7, 8.38)   # A-F列セルのサイズ設定
-cellwidth(7, 8, 40)     # G列セルのサイズ設定
+cellwidth(1, 8, 8.38)   # A-G列セルのサイズ設定
+cellwidth(8, 9, 40)     # H列セルのサイズ設定
 cellwidth(2, 3, 17.38)  # B列セルのサイズ設定
-cellwidth(8, 9, 0.38)   # H列セルのサイズ設定
-cellwidth(9, 335, 4.88) # I-LV列セルのサイズ設定
+cellwidth(9, 10, 0.38)   # I列セルのサイズ設定
+cellwidth(10, 336, 4.88) # J-LW列セルのサイズ設定
 
 for rows in range(1, 100):
     sheet.row_dimensions[rows].height = 19
@@ -45,7 +47,7 @@ for rows in sheet.iter_rows(min_row=1, min_col=1, max_row=100, max_col=9):
         if cell.row == 5:
             cell.border = Border(right=side1, bottom=side2)
         else:
-            cell.border = Border(top=None, right=side1, bottom=None, left=None)
+            cell.border = Border(right=side1)
 
 for rows in sheet.iter_rows(min_row=1, min_col=9, max_row=100, max_col=335):
     for cell in rows:
@@ -54,36 +56,38 @@ for rows in sheet.iter_rows(min_row=1, min_col=9, max_row=100, max_col=335):
         else:
             cell.border = Border(top=side3, right=side3, bottom=side3, left=side3)
 
+print("\n")
+print("#######################################")
+print("########## GetProgressChart ###########")
+print("#######################################\n")
+print("進捗管理表を自動生成するツールです。\n")
 
-# 同じ階層のディレクトリからproperty.txtをオープン
-property_data = open("property.txt", "r")
+startYear = int(input('開始年を入力してください > '))  
+startMonth = int(input('開始月を入力してください > ')) 
+startDay = int(input('開始日を入力してください > '))   
 
-# property.txtを1行ずつ要素として読み込み、PROPERTYリストを作成
-PROPERTY = property_data.readlines()
-
-startYear = int(PROPERTY[0])  # PROPERTYリストの1つ目の値を引用
-startMonth = int(PROPERTY[1]) # PROPERTYリストの2つ目の値を引用
-startDay = int(PROPERTY[2])   # PROPERTYリストの3つ目の値を引用
-
+sheet["A5"].value = "No."
 sheet["B5"].value = "タスク"
-sheet["C4"].value = "着手日"
-sheet["C5"].value = "予定"
-sheet["D5"].value = "実績"
-sheet["E4"].value = "完了日"
-sheet["E5"].value = "予定"
-sheet["F5"].value = "実績"
-sheet["G5"].value = "備考"
-sheet["I2"].value = startYear
-sheet["J2"].value = "年"
-sheet["I3"].value = startMonth
-sheet["J3"].value = "月"
+sheet["C5"].value = "担当者"
+sheet["D4"].value = "着手日"
+sheet["D5"].value = "予定"
+sheet["E5"].value = "実績"
+sheet["F4"].value = "完了日"
+sheet["F5"].value = "予定"
+sheet["G5"].value = "実績"
+sheet["H5"].value = "備考"
+sheet["J2"].value = startYear
+sheet["K2"].value = "年"
+sheet["J3"].value = startMonth
+sheet["K3"].value = "月"
 
-sheet["I4"].number_format = "d"
-sheet["I4"].value = '=DATE(I2, I3, {})'.format(startDay)
-sheet["I5"].number_format = "aaa"
-sheet["I5"].value = '=I4'
+sheet["J4"].number_format = "d"
+sheet["J4"].value = '=DATE(J2, J3, {})'.format(startDay)
+sheet["J5"].number_format = "aaa"
+sheet["J5"].value = '=J4'
 
-for rows in sheet.iter_rows(min_row=4, min_col=10, max_row=5, max_col=334):
+
+for rows in sheet.iter_rows(min_row=4, min_col=11, max_row=5, max_col=335):
     for cell in rows:
         if cell.row == 4:
             cell.number_format = 'd'
@@ -92,32 +96,40 @@ for rows in sheet.iter_rows(min_row=4, min_col=10, max_row=5, max_col=334):
             cell.number_format = 'aaa'
             cell.value = '={}'.format(sheet.cell(row=cell.row-1, column=cell.column).coordinate)
 
-for rows in sheet.iter_rows(min_row=3, min_col=11, max_row=3, max_col=334):
+
+for rows in sheet.iter_rows(min_row=3, min_col=12, max_row=3, max_col=335):
     for cell in rows:
         cell.number_format = "m"
         cell.value = '=IF(DAY({0})=1, {1},"")'.format(sheet.cell(column=cell.column, row=cell.row + 1).coordinate,
                                                       sheet.cell(column=cell.column, row=cell.row + 1).coordinate)
-
 
 def colorMake(types, start, end):
     return PatternFill(fill_type=types, start_color=start, end_color=end)
 
 
 grayfill = colorMake('solid', 'd3d3d3', 'd3d3d3')
-planfill = colorMake('solid', PROPERTY[3], PROPERTY[3]) #計画セル色。PROPERTYリストの4つ目の値を引用
-actualfill = colorMake('solid', PROPERTY[4], PROPERTY[4]) #実績セル色。PROPERTYリストの5つ目の値を引用
+planfill = colorMake('solid', '1e90ff', '1e90ff') #計画セル色。デフォルトは青。
+actualfill = colorMake('solid', 'E4007f', 'E4007f') #実績セル色。デフォルトはマゼンダ。
 
-for rows in sheet.iter_rows(min_row=6, min_col=2, max_row=100, max_col=7):
+for rows in sheet.iter_rows(min_row=6, min_col=2, max_row=100, max_col=8):
     for cell in rows:
         cell.number_format = "m/d"
-sheet.conditional_formatting.add('C6:G100', FormulaRule(formula=['NOT($F6="")'], stopIfTrue=True, fill=grayfill))
 
-sheet.conditional_formatting.add('I4:ME100', FormulaRule(formula=['OR(WEEKDAY(I$5)=1, WEEKDAY(I$5)=7)'], stopIfTrue=True, fill=grayfill))
+# 完了日の実績が入力されたら、その行のタスクをグレー色にする
+sheet.conditional_formatting.add('A6:H100', FormulaRule(formula=['NOT($G6="")'], stopIfTrue=True, fill=grayfill))
+# 土曜日と日曜日をグレー色にする
+sheet.conditional_formatting.add('J4:MF100', FormulaRule(formula=['OR(WEEKDAY(J$5)=1, WEEKDAY(J$5)=7)'], stopIfTrue=True, fill=grayfill))
+# 着手日と完了日の実績が入力されたら、着手日～完了日のセルを実績色にする（デフォルト色：マゼンタ）
+sheet.conditional_formatting.add('J6:MF100', FormulaRule(formula=['AND($E6<=J$4, $G6>=J$4)'], stopIfTrue=True, fill=actualfill))
+# 着手日と完了日の予定が入力されたら、着手日～完了日のセルを予定色にする（デフォルト色：青）
+sheet.conditional_formatting.add('J6:MF100', FormulaRule(formula=['AND($D6<=J$4, $F6>=J$4)'], stopIfTrue=True, fill=planfill))
 
-sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($D6<=I$4, $F6>=I$4)'], stopIfTrue=True, fill=actualfill))
-sheet.conditional_formatting.add('I6:ME100', FormulaRule(formula=['AND($C6<=I$4, $E6>=I$4)'], stopIfTrue=True, fill=planfill))
+# セル座標J6を固定にする
+sheet.freeze_panes = 'J6'
 
-sheet.freeze_panes = 'I6'
+# desktop_path = Desktopの絶対パス。実行環境によって変更する.
+## Windowsの場合　'C:\Users\ユーザー名\Desktop'
+## Macの場合 '/Users/ユーザ名/Desktop/' 
+#desktop_path = '/Users/seki/Desktop/'
 
-desktop_path = PROPERTY[5] # PROPERTYリストの6つ目の値を引用
-wb.save(desktop_path + '進捗管理表.xlsx')
+wb.save('進捗管理表.xlsx')
