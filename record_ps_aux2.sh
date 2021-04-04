ffa4ce5f96245848385452739e225fbf8f259fb0
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2021-04-04 20:47:49 +0900

    Update record_ps_aux2.sh

--- record_ps_aux2.sh
+++ record_ps_aux2.sh
@@ -1,6 +1,6 @@
 while true;
 do 
 date
-ps -aux | awk '{ printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);}' > ps_record_`date +%Y%m%d_%H%M%S`.csv;
+ps aux | awk '{ printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);}' > ps_record_`date +%Y%m%d_%H%M%S`.csv;
 sleep 3600;
 done; 
