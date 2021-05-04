87f117e47330601b4ad02be96cdb9f3a228b2e12
R.ishikawa <31625380+Seki14@users.noreply.github.com>
2021-05-04 21:06:41 +0900

    Add files via upload

---
+++ PerformanceTest/yes.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem Filename: yes.bat
+rem Detail: Unix/Linuxのyesコマンドの動作をバッチファイルで記載したもの。
+rem Implementer: Ryo Ishikawa
+rem Version: 1.1
+rem Last Updated: 2021/5/4
+
+rem Version History
+rem 1. Create New       R.I      2021/5/4
+
+rem メイン処理（Start）
+
+:Label
+echo y
+goto :Label
+
+rem メイン処理(End)
